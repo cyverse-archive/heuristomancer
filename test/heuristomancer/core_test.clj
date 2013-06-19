4c340c624159b4fabd41ce4deb72af32969a0bc5
Dennis Roberts <dennis@iplantcollaborative.org>
2013-06-19 15:54:12 -0700

    CORE-4446: added a some unit tests and modified ace.ebnf to match one of the oddball test files

--- test/heuristomancer/core_test.clj
+++ test/heuristomancer/core_test.clj
@@ -4,18 +4,33 @@
         [heuristomancer.loader :only [resource-reader]]))
 
 (defn- parse-test-file
+  "Parses a test file and returns the result."
   [path]
   (with-open [r (resource-reader path)]
     (identify r)))
 
 (defn- test-file-type-identification
+  "Verifies that a file type is identified correctly."
   [[path expected]]
   (testing (str path " identified as " expected)
     (is (= expected (parse-test-file path)))))
 
 (def ^:private tests
-  [["foo.csh" :csh]
-   ["foo.pl"  :perl]])
+  [["foo.csh"            :csh]
+   ["foo.tcsh"           :tcsh]
+   ["foo.sh"             :sh]
+   ["foo.bash"           :bash]
+   ["foo.pl"             :perl]
+   ["foo.py"             :python]
+   ["dna-quote.ace"      :ace]
+   ["dna-colon.ace"      :ace]
+   ["peptide-quote.ace"  :ace]
+   ["peptide-colon.ace"  :ace]
+   ["protein-quote.ace"  :ace]
+   ["protein-colon.ace"  :ace]
+   ["sequence-quote.ace" :ace]
+   ["sequence-colon.ace" :ace]
+   ["test.ace"           :ace]])
 
 (deftest file-type-identification
   (dorun (map test-file-type-identification tests)))
